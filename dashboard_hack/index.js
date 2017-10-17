e40e7e84371188abb58e4ab2ef24ec9361bb10ee
Redocram <redocram@gmail.com>
2017-10-17 17:28:47 +0200

    Incominciato a lavorare su db

---
+++ dashboard_hack/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var port = 3000;
+var Issue = require('models/issues');
+
+
+
+
+function update(obj){
+    var id = obj._id;
+    Item.findById(id, function (err, item) {
+        if(!item){
+            item.save().then(function (item) {
+                console.log('salvato');
+            }).catch(function (err) {
+                //res.status(400).send("unable to save to database");
+            });
+        }else{
+            item.item = req.body.item;
+            item.save().then(function (item) {
+                console.log('salvato');
+            }).catch(function (err) {
+                //res.status(400).send("unable to update the database");
+            });
+        }
+    });
+}

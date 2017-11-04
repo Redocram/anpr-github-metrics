2e541261cd765c7e8ab7d4b5ef90884b63c8c9b1
Redocram <redocram@gmail.com>
2017-11-04 17:08:10 +0100

    ora le chiamate (e le scritture e letture) sul db, funzionano

--- dashboard_hack/repoApi/server.js
+++ dashboard_hack/repoApi/server.js
@@ -11,12 +11,15 @@ mongoose.connect('mongodb://localhost:27017/dashboard', {useMongoClient:true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+var routes = require('./api/routes/repoRoutes');
+routes(app);
+
 app.use(function(req, res){
 	res.status(404).send({url: req.originalUrl + ' not found'});
 });
 
-var routes = require('./api/routes/repoRoutes');
-routes(app);
+
 
 app.listen(port);
 console.log('RESTful API server started on port: ' + port);

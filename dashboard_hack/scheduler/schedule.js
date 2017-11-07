666fca21446908abf3f516696c6b13e0c452ee96
Redocram <redocram@gmail.com>
2017-11-07 16:15:44 +0100

    aggiunti i parametri mlab

--- dashboard_hack/scheduler/schedule.js
+++ dashboard_hack/scheduler/schedule.js
@@ -2,9 +2,43 @@
 
 var schedule = require('node-schedule');
 let server = require('../githubApi/server');
+let http = require('http');
 
 var j = schedule.scheduleJob('*/1 * * * *', function(){
-    console.log('Controllo repo in data: ' + new Date());
-    server.github();
+    console.log('Checked repo on: ' + new Date());
+
+    http.get('http://localhost:3000/github', (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+
+        let error;
+        if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' +
+                `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+            error = new Error('Invalid content-type.\n' +
+                `Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+            console.error(error.message);
+            // consume response data to free up memory
+            res.resume();
+            return;
+        }
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                console.log(parsedData);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+    });
     //console.log('On idle...');
 });
